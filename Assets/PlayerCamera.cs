604d5f69b9650cf8b115adf926f819f5a17e3746
Anik Pal <tjdustain@gmail.com>
2025-09-05 14:48:41 +0600

    PlayerMovement with CameraFollow

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 100f;
+    private float xRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 look = context.ReadValue<Vector2>();
+
+        float mouseX = look.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = look.y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float MoveSpeed = 5f;
+
+    private Vector2 _moveInput;
+    private bool _isGrounded = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            _moveInput = context.ReadValue<Vector2>();
+        else if (context.canceled)
+            _moveInput = Vector2.zero;
+    }
+
+    private void Update()
+    {
+
+        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
+        transform.Translate(move * MoveSpeed * Time.deltaTime);
+    }
+
+}
