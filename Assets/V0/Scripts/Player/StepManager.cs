230132b2ff125992eee52be1b0c2e48b094db5be
Anik Pal <tjdustain@gmail.com>
2025-09-12 16:30:53 +0600

    fix secne

--- Assets/V0/Scripts/Player/StepManager.cs
+++ Assets/V0/Scripts/Player/StepManager.cs
@@ -7,8 +7,6 @@ public class Step
     [Tooltip("The exact component that represents this step task")]
     public StepInteractable task;
 
-    [Tooltip("Audio that will play before this step starts")]
-    public AudioClip stepAudio;
 }
 
 
